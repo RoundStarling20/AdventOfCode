30f09a7c80ee53896f1848365ae9aec1bfb61615
RoundStarling20 <88243850+RoundStarling20@users.noreply.github.com>
2023-12-03 23:28:59 -0500

    Completed day 1

--- 2023/Day1/Part1/day1.py
+++ 2023/Day1/Part1/day1.py
@@ -2,6 +2,6 @@ with open('input.txt', 'r') as file:
     count = 0
     Lines = file.readlines()
     for line in Lines:
-        string = ''.join(filter(str.isdigit, line)) #if str subset is a string, add to empty string
+        string = ''.join(filter(str.isdigit, line)) #if str subset is an int, add to empty string
         count += int(string[0] + string[-1])
     print(count)
---
+++ 2023/Day1/Part2/day1.py
@@ -0,0 +1,34 @@
+numbers = {"one" : "1", 
+           "two" : "2", 
+           "three" : "3",
+            "four" : "4",
+            "five" : "5",
+             "six" : "6", 
+             "seven" : "7", 
+             "eight" : "8", 
+             "nine" : "9"}
+
+def num2int(string:str) ->str:
+    calibration = ''
+    j = 0
+    for i in range(len(string)):
+        if string[i].isdigit():
+            calibration = calibration + string[i]
+            j = i
+        else:
+            substring = string[j:i+1]
+            for key in numbers.keys():
+                if key in substring:
+                    j = i - 1 #account for overlap twoone = 21
+                    substring = numbers[key]
+                    calibration = calibration + substring
+                    break
+    return calibration
+
+with open('input.txt', 'r') as file:
+    count = 0
+    Lines = file.readlines()
+    for line in Lines:
+        string = num2int(line)
+        count += int(string[0] + string[-1])
+    print(count)
